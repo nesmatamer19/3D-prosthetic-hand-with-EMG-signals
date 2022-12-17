28b02edb91724fcc01ad013e0b28df48cf21fcd8
MahmoudTareek <79456883+MahmoudTareek@users.noreply.github.com>
2022-12-17 21:25:49 +0200

    Update sketch_dec17a.ino
    Signed-off-by: MahmoudTareek <79456883+MahmoudTareek@users.noreply.github.com>

--- 3D hand/sketch_dec17a.ino
+++ 3D hand/sketch_dec17a.ino
@@ -5,6 +5,9 @@ Servo myservo;  // create servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
 
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
 
 for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
