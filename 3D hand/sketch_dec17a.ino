cc8a10db8a8330b24ddc960a5b1db209c84525c4
MahmoudTareek <79456883+MahmoudTareek@users.noreply.github.com>
2022-12-17 21:25:40 +0200

    Update sketch_dec17a.ino
    Signed-off-by: MahmoudTareek <79456883+MahmoudTareek@users.noreply.github.com>

--- 3D hand/sketch_dec17a.ino
+++ 3D hand/sketch_dec17a.ino
@@ -1,3 +1,11 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+
 for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
